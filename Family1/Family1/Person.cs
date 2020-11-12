9aa6524fbfaff9b84148cd6a2b6450cf01c69456
nii98 <71433716+nii98@users.noreply.github.com>
2020-11-12 19:04:21 +0500

    Add files via upload

---
+++ Family1/Family1/Person.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks.Sources;
+
+namespace Family1
+{
+    enum Sex { male, female };
+
+    class Person
+    {
+
+        //  private bool sex; // male= true female =false
+        private Sex sex;
+        private string name;
+        private Person partner { get; set; }
+        private Person dad;
+        private Person mom;
+        private List<Person> child;
+
+        public Person(string name, Sex sex)
+        {
+            this.name = name;
+            this.sex = sex;
+        }
+
+
+        public string GetPersonName()
+        {
+            return this.name;
+        }
+
+        public string GetPartnerName()
+        {
+
+            if (this.partner != null)
+            {
+                return this.partner.name;
+            }
+            else {
+                return "Not found";
+            }
+        }
+        public Person GetPartner()
+        {
+            return this.partner;
+        }
+
+
+        public void MarrriedOn(Person married)
+        {
+            if (this.partner == null)
+            {
+                this.partner = married;
+                married.partner = this;
+            }
+
+            else
+            {
+                this.partner.partner = null;
+                this.partner = married;
+                married.partner = this;
+            }
+          
+        }
+
+
+        public void SetChild(Person child)
+        {
+
+            if (this.child != null)
+            {
+                this.child.Add(child);
+            }
+            if (this.child == null)
+            {
+                List<Person> children = new List<Person>();
+                children.Add(child);
+                this.child = children;
+
+            }
+
+
+            if (this.partner != null)
+
+            {
+
+                if (this.partner.child != null)
+                {
+                    this.partner.child.Add(child);
+                }
+
+                if (this.partner.child == null)
+
+                {
+                    List<Person> children = new List<Person>();
+                    children.Add(child);
+                    this.partner.child = children;
+
+                }
+
+                if (this.partner.sex == Sex.male)
+                {
+                    child.dad = this.partner;
+                }
+
+                if (this.partner.sex == Sex.female)
+                {
+                    child.mom = this.partner;
+                }
+            }
+
+            if (this.sex == Sex.male) { child.dad = this; }
+            if (this.sex == Sex.female) { child.mom = this; }
+
+
+        }
+
+
+        private Person GetDad()
+        {
+            return this.dad;
+        }
+
+        private Person GetMom()
+        {
+            return this.mom;
+        }
+
+        public string GetParentsNames()
+
+        {
+            return this.dad.name + " " + this.mom.name;
+
+        }
+
+        private List<Person> GetBrother()
+        {
+            List<Person> brother_list = new List<Person>();
+            if (this.dad != null)
+
+            {
+                brother_list.AddRange(this.dad.child);
+
+            }
+
+            if (this.mom != null)
+            {
+                brother_list.AddRange(this.mom.child);
+
+            }
+
+
+            brother_list.Remove(this);
+            return brother_list;
+        }
+        private List<Person> FindBloodUncles()
+        {
+            List<Person> uncle_list = new List<Person>();
+            if (this.dad != null) {
+
+                if (this.dad.GetBrother() != null)
+                {
+                    uncle_list.AddRange(this.dad.GetBrother());
+                }
+            }
+            if (this.mom != null)
+            {
+                if (this.mom.GetBrother() != null)
+                {
+                    uncle_list.AddRange(this.mom.GetBrother());
+                }
+
+            }
+
+            return uncle_list;
+        }
+
+        public List<Person> GetUncles()
+        {
+            List<Person> listOfUncles = new List<Person>();
+            Person LastUncle = null;
+            foreach (Person uncles in this.FindBloodUncles())
+            {
+                listOfUncles.Add(uncles);
+                if (uncles.partner != null)
+                {
+                    listOfUncles.Add(uncles.partner);
+                }
+
+                // фильтр
+                if (uncles == this.dad || uncles == this.mom)
+                {
+                    listOfUncles.Remove(uncles);
+                    listOfUncles.Remove(uncles.partner);
+                }
+
+                if (uncles == LastUncle && (uncles.dad == LastUncle.dad || uncles.mom == LastUncle))
+                {
+
+                    listOfUncles.Remove(uncles);
+                
+                }
+                listOfUncles = listOfUncles.GroupBy(x => x.name ).Select(x => x.First()).ToList();
+               
+            }
+            
+
+            return listOfUncles;
+
+        }
+
+
+        
+
+        public void PrintUncles()
+        {
+            foreach (Person uncles in this.GetUncles())
+            {
+                Console.WriteLine(uncles.name);
+
+            }
+
+            
+        }
+
+
+        private List<Person> GetLaws()
+        {
+            List<Person> laws_list = new List<Person>();
+            if (this.partner != null)
+            {
+                laws_list.Add(this.partner.dad);
+                laws_list.Add(this.partner.mom);
+            }
+            return laws_list;
+
+        }
+
+        public void PrintLaws()
+        {
+
+            foreach (Person laws in this.GetLaws())
+            {
+                Console.WriteLine(laws.name);
+
+            }
+        }
+
+        public List<Person> GetCusins()
+        {
+            //Person LastUncle = null;
+            List<Person> cusin_list = new List<Person>();
+            List<Person> Uncles_list = this.GetUncles();
+            Person LastUncle = null;
+            foreach (Person uncle in Uncles_list)
+            {
+                if (uncle.child != null && LastUncle != uncle.partner) 
+                {
+                    cusin_list.AddRange(uncle.child);
+                    LastUncle = uncle;
+                }
+            
+                               
+            }
+            return cusin_list;
+        }
+
+
+        
+       
+        public void PrintCusins()
+        {
+
+            foreach (Person cusin in this.GetCusins())
+            {
+                Console.WriteLine(cusin.name);
+            }
+        }
+
+
+        public void PrintChild()
+        {
+            if (this.child != null)
+            {
+                foreach (Person child in this.child)
+                {
+                    Console.WriteLine(child.name);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("not found");
+
+            }
+        }
+    }
+
+  
+}
+
---
+++ Family1/Family1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Family1
+{
+    class Program
+
+
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            /* Person human1 = new Person("Bob",Sex.male);
+             Person human2 = new Person("Helena", Sex.female);
+             Person human5 = new Person("Karl", Sex.male);
+             Person human4 = new Person("Sara", Sex.female);
+             Person human3 = new Person("Dave", Sex.male );
+             Person human6 = new Person("Jonh", Sex.male);
+             Person human7 = new Person("Mary", Sex.female );
+             Person human8 = new Person("Derek", Sex.male);
+             Person human9 = new Person("Giny", Sex.female);
+             Person human10 = new Person("Liza", Sex.female);
+             Person human11 = new Person("Eric" , Sex.male);
+
+             // браки
+             human1.MarrriedOn(human2);
+            // human3.MarrriedOn(human4);
+             human6.MarrriedOn(human7);
+             human8.MarrriedOn(human9);
+
+
+
+             // Дети
+             human8.SetChild(human11);
+             human6.SetChild(human10);
+             human3.SetChild(human8);
+             human3.MarrriedOn(human4);
+             human4.SetChild(human6);
+
+             human1.SetChild(human3);
+
+             human1.MarrriedOn(human2);
+
+
+             //вывод
+           //  Console.WriteLine(human11.GetPersonName());
+            // Console.WriteLine(human11.GetParentsNames());
+             // human11.PrintUncles();
+             // human11.PrintCusins();
+             // human7.PrintLaws();
+
+               // Console.WriteLine(  human6.GetPartnerName());
+
+
+            human11.PrintUncles();
+
+             //Console.WriteLine(human1.GetPartnerName());
+
+            // human3.PrintChild();
+            */
+            // Test1
+            /* Person me = new Person("Bob", Sex.male);
+             Person father = new Person("Jack", Sex.male);
+             Person mother = new Person("Alice", Sex.female);
+             Person grandfather_by_father_side = new Person("Joe", Sex.male);
+             Person uncle_by_father_side_1 = new Person("John", Sex.male);
+             Person uncle_by_father_side_2 = new Person("Peter", Sex.male);
+             Person grandfather_by_mother_side = new Person("Jerry", Sex.male);
+             Person uncle_by_mother_side_1 = new Person("Tom", Sex.male);
+             father.MarrriedOn(mother);
+             father.SetChild(me);
+             grandfather_by_father_side.SetChild(father);
+             grandfather_by_father_side.SetChild(uncle_by_father_side_1);
+             grandfather_by_father_side.SetChild(uncle_by_father_side_2);
+             grandfather_by_mother_side.SetChild(mother);
+             grandfather_by_mother_side.SetChild(uncle_by_mother_side_1);
+             Person tetya1 = new Person("Vera", Sex.male);
+
+             uncle_by_father_side_1.MarrriedOn(tetya1);
+
+
+             me.PrintUncles();
+             */
+            // Test 2
+            /*
+            Person me = new Person("Bob", Sex.male);
+            Person father = new Person("Jack", Sex.male);
+            Person mother = new Person("Alice", Sex.female);
+            Person grandfather_by_father_side = new Person("Joe", Sex.male);
+            Person uncle_by_father_side_1 = new Person("John", Sex.male);
+            Person uncle_by_father_side_2 = new Person("Peter", Sex.male);
+            Person grandmother_by_mother_side = new Person("Jane", Sex.female);
+            Person uncle_by_mother_side_1 = new Person("Tom", Sex.male);
+            father.MarrriedOn(mother);
+            father.SetChild(me);
+            grandfather_by_father_side.SetChild(father);
+            grandfather_by_father_side.SetChild(uncle_by_father_side_1);
+            grandfather_by_father_side.SetChild(uncle_by_father_side_2);
+            grandmother_by_mother_side.SetChild(mother);
+            grandmother_by_mother_side.SetChild(uncle_by_mother_side_1);
+            me.PrintUncles();
+            */
+
+            // test 3 
+            //Person me = new Person("Bob", Sex.male);
+            //me.PrintUncles();
+
+            //test4
+            Person me = new Person("Bob", Sex.male);
+            Person father = new Person("Jack", Sex.male);
+            Person mother = new Person("Alice", Sex.female);
+            Person grandfather_by_father_side = new Person("Joe", Sex.male);
+            Person grandmother_by_father_side = new Person("Mary", Sex.female);
+            Person uncle_by_father_side_1 = new Person("John", Sex.male);
+            Person uncle_by_father_side_2 = new Person("Peter", Sex.male);
+            Person grandmother_by_mother_side = new Person("Jane", Sex.female);
+            Person uncle_by_mother_side_1 = new Person("Tom", Sex.male);
+            
+            father.MarrriedOn(mother);
+            father.SetChild(me);
+            grandfather_by_father_side.MarrriedOn(grandmother_by_father_side);
+            grandfather_by_father_side.SetChild(father);
+            grandfather_by_father_side.SetChild(uncle_by_father_side_1);
+            grandfather_by_father_side.SetChild(uncle_by_father_side_2);
+            grandmother_by_mother_side.SetChild(mother);
+            grandmother_by_mother_side.SetChild(uncle_by_mother_side_1);
+            
+            me.PrintUncles();
+        }
+    }
+}
